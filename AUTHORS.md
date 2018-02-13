70a60da68072048f5d033688ba5cc4e8869f34d3
Igor Dyatlov <dyatlov.igor@gmail.com>
2018-02-13 19:33:05 +0300

    Rebase of original script

---
+++ AUTHORS.md
@@ -0,0 +1,5 @@
+This project developed & maintained by:
+
+Sam Hewitt <sam@snwh.org>
+
+See contributor graph on GitHub for additional contributors.
--- start.sh
+++ start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # -*- Mode: sh; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 #
-# Authors base:
-#   Sam Hewitt <hewittsamuel@gmail.com>
+# Authors:
+#   Sam Hewitt <sam@snwh.org>
 #
 # Authors:
 #   Igor Dyatlov <dyatlov.igor@gmail.com>
 #
 # Description:
-#   A post-installation bash script for Ubuntu
+#   A set of post-installation script for Ubuntu and Ubuntu-based distributions
 #
 # Legal Preamble:
 #
-# This script is free software; you can redistribute it and/or modify it under
+# This project is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation; version 3.
 #
-# This script is distributed in the hope that it will be useful, but WITHOUT
+# This project is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
@@ -28,51 +28,61 @@
 tabs 4
 clear
 
-#----- Import Functions -----#
+# Import functions
 
-dir="$(dirname "$0")"
+dir=$(dirname "$0")
 
 . $dir/functions/check
 . $dir/functions/cleanup
 . $dir/functions/configure
 . $dir/functions/kernel
+. $dir/functions/password
 . $dir/functions/speedup
 . $dir/functions/thirdparty
 . $dir/functions/thirdparty-theme
 . $dir/functions/update
 
-#----- Fancy Messages -----#
-show_error(){
-echo -e "\033[1;31m$@\033[m" 1>&2
-}
-show_info(){
-echo -e "\033[1;32m$@\033[0m"
-}
-show_warning(){
-echo -e "\033[1;33m$@\033[0m"
-}
-show_question(){
-echo -e "\033[1;34m$@\033[0m"
-}
-show_success(){
-echo -e "\033[1;35m$@\033[0m"
-}
-show_header(){
-echo -e "\033[1;36m$@\033[0m"
-}
-show_listitem(){
-echo -e "\033[0;37m$@\033[0m"
+# Fancy colorful echo messages
+function echo_message(){
+	local color=$1;
+	local exp=$2;
+	if ! [[ $color =~ '^[0-9]$' ]] ; then
+		case $(echo -e $color | tr '[:upper:]' '[:lower:]') in
+			# 0 = black
+			title) color=0 ;;
+			# 1 = red
+			error) color=1 ;;
+			# 2 = green
+			info) color=2 ;;
+			# 3 = yellow
+			warning) color=3 ;;
+			# 4 = blue
+			question) color=4 ;;
+			# 5 = magenta
+			success) color=5 ;;
+			# 6 = cyan
+			header) color=6 ;;
+			# 7 = white
+			*) color=7 ;;
+		esac
+	fi
+	tput bold;
+	tput setaf $color;
+	echo $exp;
+	tput sgr0;
 }
 
+# Main
 function main {
-  eval `resize`
-  MAIN=$(whiptail \
-    --notags \
+	echo_message title "Starting 'main' function..."
+	# Draw window
+	MAIN=$(eval `resize` && whiptail \
+		--notags \
     --title 'Ubuntu Post-Install Script' \
     --menu '\nWhat would you like to do?' \
     --ok-button 'Run' \
     --cancel-button 'Quit' \
-  $LINES $COLUMNS $(( $LINES - 12 )) \
+		$LINES $COLUMNS $(( $LINES - 12 )) \
     update 'Perform system update' \
     thirdparty 'Install third-party applications' \
     thirdparty-theme 'Install third-party theme' \
@@ -80,32 +90,38 @@ function main {
     kernel 'Update system kernel' \
     cleanup 'Cleanup the system' \
     speedup 'Speed up system' \
-  3>&1 1>&2 2>&3)
+		3>&1 1>&2 2>&3)
 
-exitstatus=$?
-  if [ $exitstatus = 0 ]; then
-    $MAIN
-  else
-    quit
-  fi
+  # check exit status
+	EXITSTATUS=$?
+	if [ $EXITSTATUS = 0 ]; then
+		$MAIN
+	else
+		quit
+	fi
 }
 
 # Quit
 function quit {
-    if (whiptail --title "Quit" --yesno "Are you sure you want quit?" 10 60) then
-        echo "Exiting..."
-        show_info 'Thanks for using!'
-        exit 99
-    else
-        main
-    fi
+	echo_message title "Starting 'quit' function..."
+	if (whiptail --title "Quit" --yesno "Are you sure you want quit?" 8 64) then
+		echo "Exiting..."
+		echo_message success 'Thanks for using!'
+		exit 99
+	else
+		main
+	fi
 }
 
-#RUN
-check_dependencies
+# Welcome message
+echo_message header "Ubuntu Post-Install Script"
+# Run check
+check
+
+# Main
 while :
 do
-  main
+	main
 done
 
 #END OF SCRIPT
